8978a6fdfcc15671426c171e3661303484f64919
hilshire <hillshire.triela@gmail.com>
2021-05-26 23:20:07 +0800

    blog

---
+++ pages/blog/[id].tsx
@@ -0,0 +1,52 @@
+import Markdown from "markdown-to-jsx";
+import { getConnection } from "typeorm";
+import { useRouter } from 'next/router'
+import { Blog as BlogEntity } from "../../server/entity";
+import { getEnv } from "../../util";
+
+export function Blog({ blogJson }) {
+  const router = useRouter()
+
+  if (router.isFallback) {
+    return <div>Loading...</div>
+  }
+
+  const data: BlogEntity = JSON.parse(blogJson)
+
+  const { title = 'Ops!', context = 'something went wrong' } = data
+
+  return <Markdown>
+    {`
+# ${title}
+${context}
+      `}
+  </Markdown>
+}
+
+export function getStaticPaths() {
+  return {
+    paths: [],
+    fallback: 'blocking'
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const { id } = params
+  if (!id) {
+    return { notFound: true }
+  }
+  const connection = getConnection(getEnv());
+  const blog = await connection.getRepository<BlogEntity>(BlogEntity).findOne(params.id);
+  if (!blog) {
+    return { notFound: true }
+  }
+
+  return {
+    props: {
+      blogJson: JSON.stringify(blog)
+    },
+
+  }
+}
+
+export default Blog

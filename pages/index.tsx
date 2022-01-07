03d13f669ce9ff8dccb9be065594b8a6e4b3bc12
hilshire <hillshire.triela@gmail.com>
2022-01-07 19:25:18 +0800

    pagination

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,31 @@
 import { Blog as BlogEntity } from '@server/entity';
 import { getRepo } from '@utils';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
-export default function Home({ list }) {
+export default function Home({ list, count }) {
+  const router = useRouter();
   const blogList = JSON.parse(list);
+  const page = router.query.page as string || 1;
+  const endPage = count / 5;
+
   return (
-    <div className="blog-list">
-      {blogList.map((b) => (
-        <Link
-          key={b.id}
-          href={`/blog/${b.id}`}
-        >
-          <div className="blog-list-item">{b.title}</div>
-        </Link>
-      ))}
-    </div>
+    <>
+      <div className="blog-list">
+        {blogList.map((b) => (
+          <Link
+            key={b.id}
+            href={`/blog/${b.id}`}
+          >
+            <div className="blog-list-item">{b.title}</div>
+          </Link>
+        ))}
+        <div className="pagination">
+          { +page > 1 && <a href={`/?page=${+page - 1}`}>上一页&nbsp;&nbsp;&nbsp;</a> }
+          { (page < endPage) && <a href={`/?page=${+page + 1}`}>下一页</a> }
+        </div>
+      </div>
+    </>
   );
 }
 
@@ -24,13 +35,16 @@ export async function getServerSideProps({ query }) {
   const blog = await repo.find({
     skip: (page - 1) * 5,
     take: 5,
+    select: ['title', 'createdAt', 'lastUpdateAt', 'id'],
     order: {
       id: 'DESC',
     },
   });
+  const count = await repo.count();
   return {
     props: {
       list: JSON.stringify(blog),
+      count,
     },
   };
 }
